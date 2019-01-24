436774509a0b3a2fb8f613eccd8a4e4afb1c7d95
Vincent Mai <vincent.mai@umontreal.ca>
2019-01-24 11:41:01 -0500

    Algo running for a fixed amount of steps

--- Asst_1/Ex_1_a/main.py
+++ Asst_1/Ex_1_a/main.py
@@ -2,6 +2,7 @@ from environment import Environment
 from algorithms import ActionEliminationAlgo
 
 
+FIXED_NB_STEPS = 6000
 
 class MainLoop():
     def __init__(self):
@@ -27,7 +28,8 @@ class MainLoop():
         self.algo.update(action,reward)
 
     def findBestArm(self):
-        while not self.algo.isDone():
+        #while not self.algo.isDone():
+        while self.step < FIXED_NB_STEPS:
             self.doOneStep()
             #print("Step: " + str(self.step))
         return self.algo.result()
@@ -39,7 +41,18 @@ class MainLoop():
 
 
 if __name__ == "__main__":
-    main_loop = MainLoop()
-    result = main_loop.findBestArm()
-    print(result)
+    action_sum_per_step = [[0, 0, 0, 0, 0, 0] for k in range(FIXED_NB_STEPS)]
+
+    for i in range(1000):
+        main_loop = MainLoop()
+        result = main_loop.findBestArm()
+        action_mem = main_loop.get_action_memory()
+        for time_step in range(len(action_mem)):
+            action = action_mem[time_step]
+            try:
+                action_sum_per_step[time_step-1][action-1] += 1
+            except:
+                print("Could not with time step: " + str(time_step) + " and action: " + str(action))
+        print(result)
+    print action_sum_per_step
 
