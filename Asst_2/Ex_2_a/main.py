bcd364470c056334603bd4ba0ff80354011cace8
Brady Neal <bradyneal11@gmail.com>
2019-03-07 20:33:59 -0500

    Implement TD(lambda) and update main.py to run everything

--- Asst_2/Ex_2_a/main.py
+++ Asst_2/Ex_2_a/main.py
@@ -4,13 +4,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import gym
 from tilecoding.representation import TileCoding
-from linapprox import mc_eval, td_eval
+from linapprox import mc_eval, td_lambda_eval
 
 NUM_BINS = 10
 NUM_TILINGS = 5
 NUM_EPISODES = 200
 LRS = [1/4, 1/8, 1/16]
 NUM_SEEDS = 10
+LAMBDAS = [0, 0.3, 0.7, 0.9, 1]
+#LRS = [1/8]
+#NUM_SEEDS = 2
+#LAMBDAS = [.3]
 
 
 def policy(env, state):
@@ -70,16 +74,19 @@ if __name__ == '__main__':
 #    start_state = np.array([0, 0])  # pendulum at top
 #    start_state = np.array([np.pi/2, 0])    # pendulum to left
 #    start_state = np.array([-np.pi/2, 0])   # pendulum to right
-    value_lists = []
-    for lr in LRS:
-        value_seeds = np.zeros((NUM_SEEDS, NUM_EPISODES))
-        for seed in range(NUM_SEEDS):
-            print('lr:', lr)
-            np.random.seed(seed)
-            tc = get_tile_coding(env)
-            value_seeds[seed, :] = mc_eval(env, policy, tc, lr, start_state,
-                                           NUM_EPISODES)
-        value_lists.append(value_seeds)
-    plot_vals(value_lists, ['lr={}'.format(lr) for lr in LRS], 'Monte Carlo')
+    for lambd in LAMBDAS:
+        value_lists = []
+        for lr in LRS:
+            value_seeds = np.zeros((NUM_SEEDS, NUM_EPISODES))
+            for seed in range(NUM_SEEDS):
+                print('lr:', lr)
+                np.random.seed(seed)
+                tc = get_tile_coding(env)
+#                value_seeds[seed, :] = mc_eval(env, policy, tc, lr, start_state,
+#                                               NUM_EPISODES)
+                value_seeds[seed, :] = td_lambda_eval(env, policy, tc, lr,
+                           lambd, start_state, NUM_EPISODES, gamma=1)
+            value_lists.append(value_seeds)
+        plot_vals(value_lists, ['lr={}'.format(lr) for lr in LRS], 'TD({})'.format(lambd))
         
     
